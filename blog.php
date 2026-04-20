99ec3367c6d87f0b6eab74dec153921bf964275c
designer726 <designer@supergifts.com>
2026-04-20 17:32:24 +0530

    blog page added

---
+++ blog.php
@@ -0,0 +1,163 @@
+<?php $pagename = basename($_SERVER['PHP_SELF']); ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+     
+<?php include('common/head.php'); ?>
+    </head>
+    <body class="appear-animate">
+        
+        <!-- Page Loader -->        
+        <div class="page-loader">
+            <div class="loader">Loading...</div>
+        </div>
+        <!-- End Page Loader -->
+
+      
+        
+        <!-- Page Wrap -->
+        <div class="page" id="top">
+            
+           <?php include('common/nav.php'); ?>
+            
+           <main id="main">
+            
+            <!-- Header Section -->
+            <section class="page-section bg-gray-light-1 bg-light-alpha-90 parallax-5" style="background-image: url(images/full-width-images/section-bg-1.jpg)" id="home">
+                <div class="container position-relative pt-30 pt-sm-50">
+                    
+                    <!-- Section Content -->
+                    <div class="text-center">
+                        <div class="row">
+                            
+                            <!-- Page Title -->
+                            <div class="col-md-8 offset-md-2">
+                                    
+                                <h1 class="hs-title-1 mb-20">
+                                    <span class="wow charsAnimIn" data-splitting="chars">Blog</span>
+                                </h1>
+                                
+                                <div class="row">
+                                    <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+                                        <p class="section-descr mb-0 wow fadeIn" data-wow-delay="0.2s" data-wow-duration="1.2s">
+                                        Latest Insights, Tips, and Updates from SGIPL
+                                        </p>
+                                    </div>
+                                </div>
+                           
+                           </div>
+                           <!-- End Page Title -->
+                            
+                        </div>                            
+                    </div>
+                    <!-- End Section Content -->
+                    
+                </div>           
+            </section>
+            <!-- End Header Section -->
+            
+            
+            <!-- Section -->
+            <section class="page-section">
+                <div class="container relative">
+                    
+                    <div class="row">
+                        
+                        <!-- Content -->
+                        <div class="col-lg-8 offset-lg-2">
+                            
+                            <!-- Post 1--> 
+                            <div class="blog-item box-shadow round p-4 p-md-5">
+                                <h2 class="blog-item-title"><a href="blog_details?BlogDetails=first-post">Welcome to Our New Blog Series on Reselling Trends</a></h2>
+                                <div class="blog-item-data">
+                                    <a href="#"><i class="mi-clock size-16"></i> October 15, 2024</a>
+                                    <span class="separator">&nbsp;</span>
+                                    <a href="#"><i class="mi-user size-16"></i> SGIPL Team</a>
+                                    <span class="separator">&nbsp;</span>
+                                    <i class="mi-folder size-16"></i>
+                                    <a href="#">Blog</a>, <a href="#">Reselling</a>
+                                </div>
+                                <div class="blog-media">
+                                    <a href="blog_details?BlogDetails=first-post"><img src="images/blog/post-prev-1.jpg" alt="Blog Post 1" /></a>
+                                </div>
+                                <div class="mb-30">
+                                    <p class="mb-0">
+                                        Dive into the evolving world of reselling with our latest insights on market trends, strategies, and success stories.
+                                    </p>
+                                </div>
+                                <div class="blog-item-foot">
+                                    <a href="blog_details?BlogDetails=first-post" class="btn btn-mod btn-round btn-medium btn-gray">Read More</a>
+                                </div>
+                            </div>
+                            <!-- End Post 1 -->
+
+                            <!-- Post 2 -->
+                            <div class="blog-item box-shadow round p-4 p-md-5">
+                                <h2 class="blog-item-title"><a href="blog_details?BlogDetails=second-post">Top 5 Tips for B2B Branding Success</a></h2>
+                                <div class="blog-item-data">
+                                    <a href="#"><i class="mi-clock size-16"></i> October 10, 2024</a>
+                                    <span class="separator">&nbsp;</span>
+                                    <a href="#"><i class="mi-user size-16"></i> SGIPL Team</a>
+                                    <span class="separator">&nbsp;</span>
+                                    <i class="mi-folder size-16"></i>
+                                    <a href="#">Branding</a>, <a href="#">B2B</a>
+                                </div>
+                                <div class="blog-media">
+                                    <a href="blog_details?BlogDetails=second-post"><img src="images/blog/post-prev-2.jpg" alt="Blog Post 2" /></a>
+                                </div>
+                                <div class="mb-30">
+                                    <p class="mb-0">
+                                        Essential strategies to elevate your brand in the competitive B2B landscape.
+                                    </p>
+                                </div>
+                                <div class="blog-item-foot">
+                                    <a href="blog_details?BlogDetails=second-post" class="btn btn-mod btn-round btn-medium btn-gray">Read More</a>
+                                </div>
+                            </div>
+                            <!-- End Post 2 -->
+
+                            <!-- Post 3 -->
+                            <div class="blog-item box-shadow round p-4 p-md-5">
+                                <h2 class="blog-item-title"><a href="blog_details?BlogDetails=third-post">Future of Gifts and Stationery Industry</a></h2>
+                                <div class="blog-item-data">
+                                    <a href="#"><i class="mi-clock size-16"></i> October 05, 2024</a>
+                                    <span class="separator">&nbsp;</span>
+                                    <a href="#"><i class="mi-user size-16"></i> SGIPL Team</a>
+                                    <span class="separator">&nbsp;</span>
+                                    <i class="mi-folder size-16"></i>
+                                    <a href="#">Industry</a>, <a href="#">Trends</a>
+                                </div>
+                                <div class="blog-media">
+                                    <a href="blog_details?BlogDetails=third-post"><img src="images/blog/post-prev-3.jpg" alt="Blog Post 3" /></a>
+                                </div>
+                                <div class="mb-30">
+                                    <p class="mb-0">
+                                        Exploring upcoming innovations and opportunities in the gifts sector.
+                                    </p>
+                                </div>
+                                <div class="blog-item-foot">
+                                    <a href="blog_details?BlogDetails=third-post" class="btn btn-mod btn-round btn-medium btn-gray">Read More</a>
+                                </div>
+                            </div>
+                            <!-- End Post 3 -->
+                            
+                        </div>
+                        <!-- End Content -->
+                    
+                    </div>
+                    
+                </div>
+            </section>
+            <!-- End Section -->
+            
+            
+            <!-- Divider -->
+            <hr class="mt-0 mb-0"/>
+            <!-- End Divider -->
+            
+       
+       </main>
+            
+          <?php include('common/footer.php'); ?>
+    </body>
+</html>
